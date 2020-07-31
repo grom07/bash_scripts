cec6b83a7be4054d3af50436db7c7fc6c5bfe050
grom07 <54661967+grom07@users.noreply.github.com>
2020-07-31 12:04:23 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-TEXT_RED_B='\e[1;31m'
+TEXT_RESET='\e[0m'
+TEXT_RED='\e[1;31m'
 TEXT_GREEN='\e[1;32m'
 
 if [ -f /var/run/reboot-required ]; then
-    echo -e $TEXT_RED_B
+    echo -e $TEXT_RED
     echo 'Reboot required!'
     echo ""
     echo 'Reboot now? (y/n)' && read x && [[ "$x" == "y" ]] && sudo reboot now;
@@ -15,4 +16,5 @@ else
     echo 'No reboot required...'
     printf 'Have a good day \e[33m\U1F600\n'
     echo ""
+    echo -e $TEXT_RESET
 fi
