f85d5301439703317c0d4cc6872b9c46a2b89cf9
grom07 <54661967+grom07@users.noreply.github.com>
2020-09-12 09:15:20 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -31,4 +31,10 @@ if [ -f /var/run/reboot-required ]; then
     echo 'Reboot now? (y/n)' && read x && [[ "$x" == "y" ]] && sudo reboot now;
     echo ""
     echo -e $TEXT_RESET
+else
+    echo -e $TEXT_GREEN
+    echo 'No reboot required...'
+    printf 'Have a good day \e[33m\U1F600\n'
+    echo ""
+    echo -e $TEXT_RESET
 fi
